2e5ff970ed47f608901b9558d080e28d391d0647
Felipe da Silva Santos <felipedssantos811@gmail.com>
2022-12-01 12:08:00 -0300

    Add SwiftUI structure

---
+++ tabnews-ios-app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  tabnews-ios-app
+//
+//  Created by Felipe Santos on 01/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tabnews-ios-app/tabnews_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabnews_ios_appApp.swift
+//  tabnews-ios-app
+//
+//  Created by Felipe Santos on 01/12/22.
+//
+
+import SwiftUI
+
+@main
+struct tabnews_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
